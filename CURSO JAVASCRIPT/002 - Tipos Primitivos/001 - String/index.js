1d9ed1efb84fb66b90598ec53d67935e6fe1bc8d
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-18 11:00:44 -0300

    string

---
+++ CURSO JAVASCRIPT/002 - Tipos Primitivos/001 - String/index.js
@@ -0,0 +1,16 @@
+console.log('"Olá Mundo!"');
+console.log("Olá Mundo!");
+console.log(`Olá Mundo!`);
+
+console.log('"Texto com Aspas"')
+
+console.log(String("Olá Mundo!"))
+console.log(String(2023))
+
+/* 
+    Como podemos escrever uma string
+    'Aqui vai uma string'
+    "Aqui vai uma string"
+    `Aqui vai uma string`
+*/
+
