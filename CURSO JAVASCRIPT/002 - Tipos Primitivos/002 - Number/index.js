a9b3bce664662d435f9032703183d617e52824b0
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-18 11:13:39 -0300

    Number

---
+++ CURSO JAVASCRIPT/002 - Tipos Primitivos/002 - Number/index.js
@@ -0,0 +1,9 @@
+console.log(123);   
+console.log("123" + 1); // Concatena
+console.log("123" - 1); // Cuidado faz a operação de subtração
+console.log(Number("123") - 1); // Se precisar fazer a operação aritmética converta o tipo string para Number
+console.log(Number("123") + 1); // Se precisar fazer a operação aritmética converta o tipo string para Number
+
+console.log(typeof 123);
+console.log(typeof "123");
+console.log(typeof Number("123"))
