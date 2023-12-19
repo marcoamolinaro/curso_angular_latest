c098d09eaa1d06b040141bdae50a762c3c0970dd
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-19 18:50:07 -0300

    objeto

---
+++ CURSO JAVASCRIPT/002 - Tipos Primitivos/006 - Object/index.js
@@ -0,0 +1,38 @@
+/*
+    Objeto: Tenis (Propriedade) "cadarço", "palmilha"
+    Objeto: Celular (Propriedade) 
+    Objeto: Drone (Propriedade)
+
+    Propriedades / Atributos / Funcionalidades
+*/
+
+console.log({
+    tenis: [
+        {
+        preco: 140,
+        disponivel: true,
+        estoque: 150,
+        cadarco: {
+            cor: "rosa",
+            tamanho: "G"
+        },
+        palmilha: {
+            tamanho: 43,
+            cor: "branca"
+        }
+    },
+    {
+        preco: 30,
+        disponivel: false,
+        estoque: 14,
+        cadarco: {
+            cor: "preto",
+            tamanho: "P"
+        },
+        palmilha: {
+            tamanho: 35,
+            cor: "branca"
+        }
+    },
+    ],
+});
