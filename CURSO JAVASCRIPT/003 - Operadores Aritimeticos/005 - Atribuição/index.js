91c6fb0d11a052e19796a23103bda598e1740f58
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-19 19:24:05 -0300

    Atribuição

---
+++ CURSO JAVASCRIPT/003 - Operadores Aritimeticos/005 - Atribuição/index.js
@@ -0,0 +1,22 @@
+/*
+    = += -= *=
+*/
+
+let a = 1;
+let b = 2;
+
+//console.log(a);
+//console.log(a + b);
+
+a += 1;
+
+console.log(a);
+
+b -= 1;
+
+console.log(b);
+
+b *= 2
+
+console.log(b);
+
